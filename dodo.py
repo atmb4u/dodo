9ccb15e1895843e4890309e05d7442f58d8321d9
Anoop Thomas Mathew <atmb4u@gmail.com>
2015-03-11 21:19:26 +0530

    initial commit for dodo hacker task tracker

---
+++ dodo.py
@@ -0,0 +1,208 @@
+#/usr/bin/python
+import argparse
+import re
+import time
+import os
+
+
+DODO_FILE = os.path.join(os.getcwd(), 'DODO')
+VERSION = "0.8"
+
+
+class TerminalColors(object):
+    """
+    Color class for listing out dodos
+    """
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+    def __init__(self):
+        pass
+
+
+def parse_dodo(line):
+    if line:
+        do_id = re.search("#\d+", line).group()[1:]
+        do_status = re.search(r'\[\W+\]', line).group()[1:-1]
+        do_time = re.search(r'(<.+>)', line)
+        do_description = re.search(r'({.+})', line)
+        if do_time:
+            do_time = do_time.group().replace("<", "").replace(">", "")
+        do_user = re.search(r'(\(.+\))', line)
+        if do_user:
+            do_user = do_user.group().replace("(", "").replace(")", "")
+        if do_description:
+            do_description = do_description.group().replace("{", "").replace("}", "")
+        return {
+            "id": do_id,
+            "time": do_time,
+            "user": do_user,
+            "status": do_status,
+            "description": do_description
+        }
+
+
+def dodo_load(args):
+    global DODO_FILE
+    do_dict = {}
+    DODO_FILE = args.file or DODO_FILE
+    with open(DODO_FILE, 'r') as file_inst:
+        contents = file_inst.readlines()
+        for content in contents:
+            do_data = parse_dodo(content)
+            do_dict.update({do_data["id"]: do_data})
+    file_inst.close()
+    return do_dict
+
+
+def dodo_unload(final_do_base):
+    content = ""
+    for key, value in final_do_base.iteritems():
+        content += "#%s [%s] <%s> (%s) {%s}\n" % (value["id"], value["status"], value["time"],
+                                                  value["user"], value["description"])
+    dodo_write(content, "w")
+
+
+def dodo_init(args):
+    file_name = args.file or DODO_FILE
+    try:
+        file_inst = open(file_name, "w")
+        file_inst.close()
+        print "Successfully initialized DoDo"
+    except IOError:
+        print "Cannot create file in the following location: %s" % file_name
+
+
+def dodo_write(content, mode="a"):
+    global DODO_FILE, do_base
+    file_inst = open(DODO_FILE, mode)
+    file_inst.write(content)
+    file_inst.close()
+    print "Dodo updated."
+
+
+def dodo_change_status(args, mod_do_base, status):
+    if not args.id:
+        print "ID (-id) can't be empty"
+        return
+    do_entry = mod_do_base.get(args.id)
+    if do_entry:
+        do_entry["status"] = status
+    else:
+        if not args.desc:
+            print "Description (-d) can't be empty"
+            return
+        do_id = str(len(mod_do_base) + 1)
+        do_description = args.desc
+        do_user = args.user
+        do_time = args.time or time.strftime("%d-%m-%y %H:%M", time.localtime())
+        mod_do_base[do_id] = {
+            "id": do_id,
+            "time": do_time,
+            "user": do_user,
+            "status": status,
+            "description": do_description
+        }
+    dodo_unload(mod_do_base)
+    return
+
+
+def dodo_add(args):
+    """
+    + add/proposed
+    * accepted
+    - rejected
+    # working
+    . complete
+    """
+    if args.operation in ["add", "propose", "c"]:
+        do_id = str(len(do_base) + 1)
+        do_description = args.desc
+        do_user = args.user
+        do_time = args.time or time.strftime("%d-%m-%y %H:%M", time.localtime())
+        do_base[do_id] = {
+            "id": do_id,
+            "time": do_time,
+            "user": do_user,
+            "status": "+",
+            "description": do_description
+        }
+        dodo_unload(do_base)
+    elif args.operation == "accept":
+        dodo_change_status(args, do_base, "*")
+    elif args.operation == "reject":
+        dodo_change_status(args, do_base, "-")
+    elif args.operation == "workon":
+        dodo_change_status(args, do_base, "#")
+    elif args.operation == "finish":
+        dodo_change_status(args, do_base, ".")
+    elif args.operation in ["remove" or "d"]:
+        try:
+            do_base.pop(args.id)
+        except KeyError:
+            print "No task with id %s" % args.id
+        dodo_unload(do_base)
+    return
+
+
+def dodo_list():
+    global do_base
+    print "%s%sID\tStatus\t\tDate(-t)\tOwner(-u)\t\tDescription (-d)\n%s" % (TerminalColors.BOLD,
+                                                                             TerminalColors.UNDERLINE,
+                                                                             TerminalColors.END)
+    for key, value in do_base.iteritems():
+        color = TerminalColors.YELLOW
+        if value["status"] == ".":
+            color = TerminalColors.GREEN
+        elif value["status"] in ["-", 'x']:
+            color = TerminalColors.RED
+        elif value["status"] == "#":
+            color = TerminalColors.YELLOW
+        elif value["status"] == "+":
+            color = TerminalColors.BLUE
+        user = value["user"] if value["user"] != "None" else "anonymous"
+        print "%s%s\t[%s]\t\t%s\t(%s)\t\t%s%s" % (color, value["id"], value["status"], value["time"],
+                                                  user, value["description"], TerminalColors.END)
+    print "%sAvailable Operations: c accept propose reject workon finish remove d\n" \
+          "Available Options: -id -d(description) -u(user) -t(time) -f(file) %s" % (
+              TerminalColors.BOLD, TerminalColors.END)
+
+
+def dodo_switch(args):
+    global do_base
+    if args.operation == "init":
+        dodo_init(args)
+    elif args.operation in ['add', 'propose', 'accept', 'reject', 'workon', 'finish', 'remove', "c", "d"]:
+        dodo_add(args)
+    else:
+        dodo_list()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("operation", type=str,
+                        help="List all existing dodos add, propose, accept, reject, workon, finish, remove")
+    parser.add_argument("-d", "--desc", "--description", type=str,
+                        help="Task Description")
+    parser.add_argument("-u", "--user", type=str,
+                        help="User ID")
+    parser.add_argument("-t", "--time", type=str,
+                        help="Expected/Completed Date - 11-03-2015")
+    parser.add_argument("--id", type=str,
+                        help="List all existing dodos")
+    parser.add_argument("--file", type=str,
+                        help="DODO filename")
+    arguments = parser.parse_args()
+    global do_base
+    do_base = {}
+    if arguments.operation == "init":
+        dodo_init(arguments)
+    else:
+        do_base = dodo_load(arguments)
+    dodo_switch(arguments)
---
+++ setup.py
@@ -0,0 +1,96 @@
+# Dodo setup.py script
+#
+# It doesn't depend on setuptools, but if setuptools is available it'll use
+# some of its features, like package dependencies.
+
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
+import os
+import sys
+import dodo
+
+
+class OsxInstallData(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': OsxInstallData}
+else:
+    cmdclasses = {'install_data': install_data}
+
+
+def full_split(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return full_split(head, [tail] + result)
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+
+def is_not_module(filename):
+    return os.path.splitext(filename)[1] not in ['.py', '.pyc', '.pyo']
+
+scripts = ['dodo']
+
+version = dodo.VERSION
+
+setup_args = {
+    'name': 'dodo',
+    'version': version,
+    'url': 'http://atmb4u.github.io/dodo',
+    'description': 'Task Management for Hackers',
+    'author': 'Anoop Thomas Mathew',
+    'author_email': 'atmb4u@gmail.com',
+    'license': 'BSD',
+    'packages': packages,
+    'cmdclass': cmdclasses,
+    'data_files': data_files,
+    'scripts': scripts,
+    'include_package_data': True,
+    'classifiers': [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(**setup_args)

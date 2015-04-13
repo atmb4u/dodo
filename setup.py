f392d51acd5742d3b3a0214a27b2c6b24a37f0a0
Dan Ross <dan@rosspixelworks.com>
2015-04-13 16:58:20 -0500

    2to3 changes to dodo.py and setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def full_split(path, result=None):
 # Tell distutils to put the data_files in platform-specific installation
 # locations. See here for an explanation:
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
+for scheme in list(INSTALL_SCHEMES.values()):
     scheme['data'] = scheme['purelib']
 
 # Compile the list of packages available, because distutils doesn't have
